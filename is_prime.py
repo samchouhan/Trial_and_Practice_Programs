870292760b81a30c94a4c6fc5a2910a33659885b
samchouhan <sakshamchouhan8@gmail.com>
2026-09-09 09:56:40 +0530

    Refactor prime number logic and class structure in is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -1,13 +1,11 @@
-a=numbers= int(input("Enter a number: "))
-while numbers==True:
-  continue
-  a+=1
-  
-if numbers%2 == 0:
-    numbers=False 
-    
-elif numbers%numbers==0:
-    numbers=True
-    
-else:
-  print("Type a valid number!")
+#A prime number is a natural number greater than 1 that cannot be formed by multiplying two smaller natural numbers. A prime number is only divisible by 1 and itself. The first few prime numbers are 2, 3, 5, 7, 11, 13, 17, 19, 23, and so on.
+#So prime number can only be formed by multiplying 1 and itself.
+
+class Prime:
+    def __init__(self, number):
+        self.number = number
+        
+    def is_prime(self):
+        if self.number <= 1:
+            return False
+        
